e0c88a881c75b0cef00d9a04d64aced3cce6c2de
praiztech <praisefagbabiaka@gmail.com>
2025-06-08 23:53:02 +0100

    implement Layout and Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import svgr from 'vite-plugin-svgr';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [svgr()],
+  },
+});
---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import OpenMenuIcon from "../assets/open-menu.svg?react";
+import CloseMenuIcon from "../assets/close-menu.svg?react";
+
+export default function MenuButton({ classes }: { classes: string }) {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const toggleMenu = () => setIsOpenMenu((prev) => !prev);
+
+  return isOpenMenu ? (
+    <button
+      type="button"
+      aria-expanded="true"
+      className={classes}
+      onClick={toggleMenu}
+    >
+      <span className="visually-hidden">Menu</span>
+      <CloseMenuIcon aria-hidden={true} />
+    </button>
+  ) : (
+    <button
+      type="button"
+      aria-expanded="false"
+      className={classes}
+      onClick={toggleMenu}
+    >
+      <span className="visually-hidden">Menu</span>
+      <OpenMenuIcon aria-hidden={true} />
+    </button>
+  );
+}
